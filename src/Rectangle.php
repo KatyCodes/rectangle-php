022d7006821d20ab85898d38523682020b4fc12c
Katy Henning and Stephen Burden <stephen@>
2016-09-06 13:54:57 -0700

    make a rectangle php page with all files

---
+++ src/Rectangle.php
@@ -0,0 +1,44 @@
+<?php
+    class Rectangle
+    {
+        private $length;
+        private $width;
+
+        function __construct($length, $width)
+        {
+            $this->length = $length;
+            $this->width = $width;
+        }
+
+        function isSquare()
+        {
+            if($this->length == $this->width) {
+              return true;
+            } else {
+              return false;
+            }
+        }
+
+        function getArea()
+        {
+            return $this->length * $this->width;
+        }
+
+        function setLength($new_length)
+        {
+            $this->length = (float) $new_length;
+        }
+
+        function getLength($new_length)
+        {
+            $this->length = (float) $new_width;
+        }
+
+        function getWidth()
+        {
+            return $this->width;
+        }
+
+    }
+
+ ?>
